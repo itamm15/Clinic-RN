4b7621552d36c35806b34383464cc78d7d44d369
itamm15 <osinski.mateusz15@gmail.com>
2025-04-12 10:12:35 +0200

    add missing cursor pointer

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -62,6 +62,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 4,
+    cursor: 'pointer',
   },
   row: {
     flexDirection: 'row',
