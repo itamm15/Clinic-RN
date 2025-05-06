dad598dac488181d29b08cc8c6742eee485da3ff
itamm15 <osinski.mateusz15@gmail.com>
2025-05-06 22:17:32 +0200

    chore: add info about no visits

--- mobile/app/(tabs)/allEvents.tsx
+++ mobile/app/(tabs)/allEvents.tsx
@@ -14,6 +14,14 @@ export default function AllEventsScreen() {
     );
   }
 
+  if (visits.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.noVisits}>Brak dostępnych wizyt.</Text>
+      </View>
+    );
+  }
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {visits.map((appointment) => {
@@ -69,4 +77,11 @@ const styles = StyleSheet.create({
     fontSize: 13,
     color: '#007AFF',
   },
+  noVisits: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#666',
+    textAlign: 'center',
+    marginTop: 48,
+  }
 });
